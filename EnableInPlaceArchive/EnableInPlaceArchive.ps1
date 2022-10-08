9b30a1b5c6d2be78137f318706cdd86e73d5c018
unknown <tayfuntech@outlook.com.tr>
2022-10-08 18:37:36 +0300

    itinial commit

--- EnableInPlaceArchive/EnableInPlaceArchive.ps1
+++ EnableInPlaceArchive/EnableInPlaceArchive.ps1
@@ -4,39 +4,45 @@ twitter: @tayfuntech
 linkedin: www.linkedin.com/in/tayfuntek/
 #>
 
+#The following commands will install and import the Exchange Onlime Management Module.
+
 Install-Module -Name ExchangeOnlineManagement -RequiredVersion 2.0.5
 Set-ExecutionPolicy RemoteSigned
 Import-Module ExchangeOnlineManagement
 
 if ( Connect-ExchangeOnline )
 {
-    Write-Output "Exchange Online'a bağlanıldı"
+    Write-Output "Connected to Exchange Online"
 }
 
+#In this section, type the "Display Name" of the O365 user
 
-$mailuser= "Office Apps"
+$mailuser= "Display Name"
+
+
+#In this section, you can define when mailbox items move to the archive by changing the Retention Tag day count.
 
 if ( New-RetentionPolicyTag "Archive365Tag" -Type All -RetentionEnabled $true -AgeLimitForRetention 365 -RetentionAction MovetoArchive )
 {
-    Write-Output "Saklama etiketi oluşturuldu"
+    Write-Output "The Retention Tag has been created successfully."
 }
 else {
-    Write-Host "Saklama etiketi zaten var. Bir şey yapmanıza gerek yok"
+    Write-Host "The Retention Tag already exists. No action is needed."
 }
 
-
+#This command creates a new Retention policy which includes the Retention Tag we defined above.
 if ( New-RetentionPolicy "Archive365Policy" -RetentionPolicyTagLinks "Archive365Tag" )
 {
-    Write-Output "Saklama ilkesi oluşturuldu"
+    Write-Output "The retention policy has been created successfully."
 }
 else {
-    Write-Host "Saklama ilkesi zaten var. Bir şey yapmanıza gerek yok"
+    Write-Host "The Retention policy already exists. No action is needed."
 }
 
-
+#Assinging the Retention Policy to the specified user.
 if ( Set-Mailbox $mailuser -RetentionPolicy "Archive365Policy" )
 {
-    Write-Output "Saklama ilkesi kullanıcıya atandı"
+    Write-Output "Retention policy assigned to user."
 }
 
 function Green
@@ -44,9 +50,13 @@ function Green
     process { Write-Host $_ -ForegroundColor Green }
 }
 
+#Activating the archive
 if ( Enable-Mailbox $mailuser -Archive )
 {
-    Write-Output "Arşiv Etkinleştirildi!" | Green
+    Write-Output "In-place archive enabled successfully.!" | Green
 }
 
 Get-Mailbox $mailuser | Format-List Name,*Archive**
+
+#It terminates the Exchange Online session.
+Disconnect-ExchangeOnline -confirm:$false
